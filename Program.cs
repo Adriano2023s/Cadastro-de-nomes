8158ce323b841fc4f7171a7f8b3a69127c5d44fc
Adriano Sousa <adrianodesousa1998@gmail.com>
2024-08-09 22:40:41 -0300

    Primeira parte da atividade, primeiro insert feito na lista de nomes

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace AdicionaNome
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] nome = new string[10];
+
+            AddNome("Adriano", nome);
+        }
+
+        public static void AddNome(string nome, string[] ListaNomes)
+        {
+            if (nome == null)
+            {
+                Console.WriteLine("Nome inválido, tente novamente");
+                return;
+            }
+
+            for (int i = 0; i < ListaNomes.Length; i++)
+                {
+                if (ListaNomes[i] == null)
+                {
+                    ListaNomes[i] = nome;
+                    Console.WriteLine($"O nome: {nome}, foi adicionado na pozição: {i}");
+                    return;
+                }
+            }
+
+        }
+    }
+}
